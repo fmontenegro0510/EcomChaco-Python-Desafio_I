5b9e2ef5622e133ee840b894c982614525bd17db
Francisco Montenegro <franciscomontenegro22@gmail.com>
2024-10-12 16:22:21 -0300

    work in leer_datos

---
+++ scripts/AnalizadorVacunacion.py
@@ -0,0 +1,32 @@
+import csv
+import os
+
+class AnalizadorVacunacion:
+    def __init__(self, archivo):
+        self.datos = self.leer_datos(archivo)
+        self.registros_erroneos = []
+
+    def leer_datos(self, archivo):
+        datos = []
+        with open(archivo, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                datos.append(row)
+        return datos
+
+    def validar_datos(self):
+        for registro in self.datos:
+            if registro['sexo'] not in ['M', 'F']:
+                registro['OBSERVACIÓN'] = 'Sexo inválido'
+                self.registros_erroneos.append(registro)
+
+    def contar_dosis(self):
+        pass
+
+    def guardar_resultados(self):
+        pass
+
+if __name__ == "__main__":
+    analizador = AnalizadorVacunacion('../data/modelo_muestra.csv')
+    analizador.validar_datos()
+
